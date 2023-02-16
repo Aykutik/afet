c801a6e4c4121865d559ce22c8cb0c681e7e8c66
Aykut Adil GÜNHAN <aykutik@msn.com>
2023-02-16 11:33:50 +0300

    Harita ekran boyutuna göre ayarlandı

--- pages/harita/index.jsx
+++ pages/harita/index.jsx
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-function harita() {
+function Harita() {
+  useEffect(() => {
+    const iframe = document.querySelector("#harita-iframe");
+
+    if (iframe) {
+      const iframeWidth = window.innerWidth;
+      const iframeHeight = window.innerHeight;
+      iframe.width = iframeWidth;
+      iframe.height = iframeHeight;
+    }
+  }, []);
+
   return (
     <div className="w-screen flex items-center justify-center">
       <div>
         <iframe
+          id="harita-iframe"
           src="https://www.google.com/maps/d/u/1/embed?mid=1KaSZXEaG4D6ItpVSKmeUVgQgQL1P9Zg&ehbc=2E312F"
-          width="1250"
-          height="700"
+          width="100%"
+          height="100%"
         ></iframe>
       </div>
     </div>
   );
 }
 
-export default harita;
+export default Harita;
